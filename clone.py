0173a6c63be7ce3bb29736dde38b4c65f1d296ac
Jack Wang <jackwang@gmail.com>
2017-08-23 10:31:12 +0000

    more epochs, no augmentation

--- clone.py
+++ clone.py
@@ -28,8 +28,8 @@ for image, measurement in zip(images, measurements):
   augmented_images.append(cv2.flip(image, 1))
   augmented_measurements.append(measurement*-1)
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+X_train = np.array(images)
+y_train = np.array(measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
@@ -37,7 +37,7 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
 model.add(Convolution2D(6, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6, 5, 5, activation='relu'))
@@ -48,6 +48,6 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
 
 model.save('model.h5')
