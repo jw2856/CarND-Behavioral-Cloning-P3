c6daeb5a50323048e3301b9ce70eb92f4ad347d5
Jack Wang <jackwang@gmail.com>
2017-08-26 06:02:36 +0000

    updated model with nvidia architecture

--- clone.py
+++ clone.py
@@ -1,6 +1,7 @@
 import csv
 import cv2
 import numpy as np
+import random
 
 lines = []
 
@@ -22,6 +23,10 @@ for index, line in enumerate(lines):
   if index == 0:
     continue
 
+  steering_center = float(line[3])
+  if (steering_center <= 0.85 and random.random() < 0.7):
+    continue
+
   center_img = get_img(line[0])
   left_img = get_img(line[1])
   right_img = get_img(line[2])
@@ -33,7 +38,7 @@ for index, line in enumerate(lines):
   steering_left = steering_center + correction
   steering_right = steering_center - correction
   
-  measurements.append([steering_center, steering_left, steering_right])
+  measurements.extend([steering_center, steering_left, steering_right])
 
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
@@ -51,18 +56,22 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160, 320, 3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+# model.add(MaxPooling2D())
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+# model.add(MaxPooling2D())
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=8)
 
 model.save('model.h5')
